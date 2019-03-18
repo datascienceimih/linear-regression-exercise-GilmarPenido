d77f16ef5786b0954dc27d67fc31418d9d0d563a
gilmarpenido <gilmar.penido@gmail.com>
2019-03-17 22:26:25 -0300

    removendo arquivos desnecessarios

--- exercicio-avaliativo.r
+++
@@ -1,9 +0,0 @@
-#install.packages("dplyr")
-library(dplyr)
-
-install.packages('ISLR')
-install.packages("readr")
-library(readr)
-
-boston = read_csv("https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv")
-head(boston)
